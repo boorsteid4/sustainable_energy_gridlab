cb89d6c0bf67c5a913c1186f768f3e0a3338899f
boorsteid4 <boorsteid4@students.rowan.edu>
2017-10-03 14:30:30 -0400

    Changed repo

--- parseCSV/main.cpp
+++ parseCSV/main.cpp
@@ -3,49 +3,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
-
-using namespace std;
-
-class CSVRow
-{
-public:
-	std::string const& operator[](std::size_t index) const
-	{
-		return m_data[index];
-	}
-	std::size_t size() const
-	{
-		return m_data.size();
-	}
-	void readNextRow(std::istream& str)
-	{
-		std::string         line;
-		std::getline(str, line);
-
-		std::stringstream   lineStream(line);
-		std::string         cell;
-
-		m_data.clear();
-		while (std::getline(lineStream, cell, ','))
-		{
-			m_data.push_back(cell);
-		}
-		// This checks for a trailing comma with no data after it.
-		if (!lineStream && cell.empty())
-		{
-			// If there was a trailing comma then add an empty element.
-			m_data.push_back("");
-		}
-	}
-private:
-	std::vector<std::string>    m_data;
-};
-
-std::istream& operator >> (std::istream& str, CSVRow& data)
-{
-	data.readNextRow(str);
-	return str;
-}
+#include "parseCSV.h"
 
 class CSVIterator
 {
@@ -73,3 +31,13 @@ private:
 	CSVRow              m_row;
 };
 
+
+int main()
+{
+	std::ifstream       file("Power Flow data needed.xlsx - Sheet1.csv");
+
+	for (CSVIterator loop(file); loop != CSVIterator(); ++loop)
+	{
+		std::cout << "4th Element(" << (*loop)[3] << ")\n";
+	}
+}
