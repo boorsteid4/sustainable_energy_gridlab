88ad93321609bc9eab2f439b07d2310d8b82d8fe
boorsteid4 <boorsteid4@students.rowan.edu>
2017-10-05 14:00:10 -0400

    Using Fast CPP CSV Parser

--- parseCSV/main.cpp
+++ parseCSV/main.cpp
@@ -3,41 +3,14 @@
 #include <string>
 #include <fstream>
 #include <vector>
-#include "parseCSV.h"
+# include "parseCSV.h"
 
-class CSVIterator
+int main() 
 {
-public:
-	typedef std::input_iterator_tag     iterator_category;
-	typedef CSVRow                      value_type;
-	typedef std::size_t                 difference_type;
-	typedef CSVRow*                     pointer;
-	typedef CSVRow&                     reference;
-
-	CSVIterator(std::istream& str) :m_str(str.good() ? &str : NULL) { ++(*this); }
-	CSVIterator() :m_str(NULL) {}
-
-	// Pre Increment
-	CSVIterator& operator++() { if (m_str) { if (!((*m_str) >> m_row)) { m_str = NULL; } }return *this; }
-	// Post increment
-	CSVIterator operator++(int) { CSVIterator    tmp(*this); ++(*this); return tmp; }
-	CSVRow const& operator*()   const { return m_row; }
-	CSVRow const* operator->()  const { return &m_row; }
-
-	bool operator==(CSVIterator const& rhs) { return ((this == &rhs) || ((this->m_str == NULL) && (rhs.m_str == NULL))); }
-	bool operator!=(CSVIterator const& rhs) { return !((*this) == rhs); }
-private:
-	std::istream*       m_str;
-	CSVRow              m_row;
-};
-
-
-int main()
-{
-	std::ifstream       file("Power Flow data needed.xlsx - Sheet1.csv");
-
-	for (CSVIterator loop(file); loop != CSVIterator(); ++loop)
-	{
-		std::cout << "4th Element(" << (*loop)[3] << ")\n";
+	io::CSVReader<3> in("datareq.csv");
+	in.read_header(io::ignore_extra_column, "vendor", "size", "speed");
+	std::string vendor; int size; double speed;
+	while (in.read_row(vendor, size, speed)) {
+		// do stuff with the data
 	}
 }
